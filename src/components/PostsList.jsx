68cf7c43496509eff386b8fc8ba4950aefc790ad
mattx2 <mattx2@gmail.com>
2024-09-24 21:43:46 +0800

    commit 1

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,22 +4,12 @@ import NewPost from './NewPost';
 import Modal from './Modal';
 import classes from './PostsList.module.css';
 
-function PostsList() {
-  const [modalIsVisible, setModalIsVisible] = useState(true);
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+function PostsList({ isPosting, onStopPosting }) {
+ const [posts, setPosts] = useState([]);
 
-function hideModalHandler() {
-  setModalIsVisible(false);
-}
-
-function bodyChangeHandler(event) {
-  setEnteredBody(event.target.value);
-}
-
-function authorChangeHandler(event) {
-  setEnteredAuthor(event.target.value);
-}
+ function addPostHander(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+ }
 
 // let modalContent;
 
@@ -36,19 +26,24 @@ function authorChangeHandler(event) {
 
   return (
     <>
-    {modalIsVisible && (
-      <Modal onClose={hideModalHandler}> 
-          <NewPost 
-            onBodyChange={bodyChangeHandler} 
-            onAuthorChange={authorChangeHandler} 
-          />
+    {isPosting && (
+      <Modal onClose={onStopPosting}> 
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHander} />
       </Modal>
     )}
-
+    {posts.length > 0 && (
     <ul className={classes.posts}>
-      <Post author={enteredAuthor} body={enteredBody} />
-      <Post author="Manuel" body="Check out the full course!" />
+      {posts.map((post) => (
+        <Post key={post.body} author={post.author} body={post.body} />
+        ))}
     </ul>
+    )}
+    {posts.length === 0 && (
+       <div style={{ textAlign: 'center', color: 'white' }}>
+        <h2>There are no posts yet.</h2>
+        <p>Start adding some!</p>
+       </div>
+      )}
     </>
   );
 }
